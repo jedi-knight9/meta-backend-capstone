0fecd9ce3f283bf4c6a435a43c89cc07a3581470
Shravya Chepa <shravyachepa@shravya.local>
2024-01-30 14:16:16 -0800

    upload project files

---
+++ littlelemon/restaurant/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class MenuItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
+    
+
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+    
+    def __str__(self):
+        return self.name
